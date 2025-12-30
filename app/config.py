221f677ba8092aaf9a24bdf9a8122028728aaec3
Estacio <estaciodifabio@hotmail.com>
2025-12-30 15:10:25 -0300

    chore: add vars

--- app/config.py
+++ app/config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SERVICE_PRODUCTION_URL = os.getenv("SERVICE_PRODUCTION_URL", "http://production_app:8080")
-SERVICE_ORDER_URL = os.getenv("SERVICE_ORDER_URL", "http://order_app:8080")
-SERVICE_PAYMENT_URL = os.getenv("SERVICE_A_URL", "http://payment_app:8080")
+SERVICE_PRODUCTION_URL = os.getenv("SERVICE_PRODUCTION_URL")
+SERVICE_ORDER_URL = os.getenv("SERVICE_ORDER_URL")
+SERVICE_PAYMENT_URL = os.getenv("SERVICE_PAYMENT_URL")
 
 REQUEST_TIMEOUT = int(os.getenv("REQUEST_TIMEOUT", 100))
